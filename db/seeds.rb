f3ff8f132d0c10be9e512cb6f204a0ac88e1a695
Paulo Henrique Deléo <paulodeleo@gmail.com>
2018-10-11 06:15:17 -0300

    Adding sample seed data from the specifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tag1 = Tag.create! name: 'Tag1'
+tag2 = Tag.create! name: 'Tag2'
+tag3 = Tag.create! name: 'Tag3'
+tag4 = Tag.create! name: 'Tag4'
+tag5 = Tag.create! name: 'Tag5'
+
+FileUpload.create! name: 'File 1', tags: [tag1, tag2, tag3, tag5]
+FileUpload.create! name: 'File 2', tags: [tag2]
+FileUpload.create! name: 'File 3', tags: [tag2, tag3, tag5]
+FileUpload.create! name: 'File 4', tags: [tag2, tag3, tag4, tag5]
+FileUpload.create! name: 'File 5', tags: [tag3, tag4]
